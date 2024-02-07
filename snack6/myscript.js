7a56f823afd2707df6fb2b743c0c3feaf4e66bc4
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-07 15:23:09 +0100

    snack6 completo

--- snack6/myscript.js
+++ snack6/myscript.js
@@ -1,17 +1,40 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
 
-const userNumber = Number(prompt("Inserisci un numero"));
+
 
 let sum = 0;
+let myButton = document.querySelector(".btn").addEventListener("click",
+    function () {
+        let resultElement = document.querySelector("p");
+        
+        for (let i = 1; i <= 10; i++) {
+            sum += i;
+        }
 
-for(let i = 1; i <= userNumber; i++) {
+        let average = sum / 10;
+
+        resultElement.innerHTML += "La somma dei primi 10 numeri è: " + sum + "<br>";
+        resultElement.innerHTML += "La media dei primi 10 numeri è: " + average + "<br>";
+    }
+);
+
+
+// altro procedimento
+
+
+
+// const userNumber = Number(prompt("Inserisci un numero"));
+
+// let sum = 0;
+
+// for(let i = 1; i <= userNumber; i++) {
     
-    sum += i;
+//     sum += i;
 
-}
+// }
 
 
-console.log(`Somma dei primi ${userNumber} numeri: ${sum}`);
+// console.log(`Somma dei primi ${userNumber} numeri: ${sum}`);
 
-console.log(`Media dei primi ${userNumber} numeri: ${sum / userNumber}`);
+// console.log(`Media dei primi ${userNumber} numeri: ${sum / userNumber}`);
